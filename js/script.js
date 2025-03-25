1025c27cf6d4c81b2598047fa3b00583050b756c
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-03-25 16:59:07 +0000

    Salary work

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yi.zhou
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+"use strict"
+
+function CalucalteTaxIncome() {
+  // Setting the variables
+  const TaxAmount = 0.18;
+  let HourlyWorked = parseFloat(document.getElementById("hourly-worked").value);
+  let HourlyWage = parseFloat(document.getElementById("hourly-wage").value);
+  let TakeHomePay = (HourlyWorked * HourlyWage) * (1.00 - 0.18)
+  let Taxes = (HourlyWorked * HourlyWage) * TaxAmount;
+
+  // currency formatting
+  let formattedPrice = new Intl.NumberFormat('en-CA', {
+    style: 'currency',
+    currency: 'CAD',
+  });
+
+  // calculations
+  document.getElementById("result").innerHTML =
+    `Take-home Pay: ${formattedPrice.format(TakeHomePay)} <br>
+    Taxes: ${formattedPrice.format(Taxes)}`;
 }
